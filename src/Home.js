e3eb5ffceedb3d670a9cae68281def9a3950d408
Joe Young <musicjoeyoung@gmail.com>
2021-04-29 12:54:14 -0400

    home page skeleton

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,78 @@
 import React, { Component } from "react";
 import HomeCarousel from "./HomeCarousel";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <br />
         <HomeCarousel />
+        <br />
+        <br />
+        <br />
+        <div className="signuplink">sign up for our newsletter</div>
+        <br />
+        <br />
+        <br />
+        <div className="about">
+          <div className="aboutimage"></div>
+          <div>about carousel</div>
+        </div>
+        <br />
+        <br />
+        <br />{" "}
+        <div className="ensembles">
+          <div>ensemble 1</div>
+          <div>ensemble 2</div>
+          <div>ensemble 3</div>
+          <div>ensemble 4</div>
+          <div>ensemble 5</div>
+          <div>ensemble 6</div>
+          <div>changing image</div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div className="events">
+          <div>embedded promo video</div>
+          <div>events 1</div>
+          <div>events 2</div>
+          <div>events 3</div>
+          <button>request the band</button>
+        </div>
+        <br />
+        <br />
+        <br />{" "}
+        <div className="media">
+          <div>circle videos</div>
+          <div>circle pictures</div>
+          <div>circle articles</div>
+          <div>embedded video</div>
+        </div>
+        <br />
+        <br />
+        <br />{" "}
+        <div className="jobs">
+          <div>
+            vacancies
+            <div>instrument</div>
+            <div>instrument</div>
+            <div>instrument</div>
+          </div>
+          <div>
+            Audition info.
+            <div>description</div>
+            <div>samples</div>
+            <div>media resources</div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <Footer />
       </div>
     );
   }
--- src/HomeCarousel.js
+++ src/HomeCarousel.js
@@ -7,12 +7,15 @@ import img2 from "./images/armyband2.jpeg";
 import img3 from "./images/armyband3.jpeg";
 export default function HomeCarousel() {
   return (
-    <div>
-      <AliceCarousel autoPlay autoPlayInterval="3000" infinite>
-        <img src={img1} alt="" className="sliderimg" />
-        <img src={img2} alt="" className="sliderimg" />
-        <img src={img3} alt="" className="sliderimg" />
-      </AliceCarousel>
-    </div>
+    <AliceCarousel
+      autoPlay
+      autoPlayInterval="5000"
+      infinite
+      animationType="fadeout"
+    >
+      <img src={img1} alt="" className="sliderimg" />
+      <img src={img2} alt="" className="sliderimg" />
+      <img src={img3} alt="" className="sliderimg" />
+    </AliceCarousel>
   );
 }
