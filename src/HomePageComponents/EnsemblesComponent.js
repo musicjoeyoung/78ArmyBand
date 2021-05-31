58bde3d4a64f1832fded77576c197481d44f950d
Joe Young <musicjoeyoung@gmail.com>
2021-05-30 21:50:29 -0400

    images

--- src/HomePageComponents/EnsemblesComponent.js
+++ src/HomePageComponents/EnsemblesComponent.js
@@ -1,4 +1,26 @@
 import React, { Component } from "react";
+import ensemble1 from "../images/armyband1.jpg";
+import ensemble2 from "../images/armyband2.jpeg";
+import ensemble3 from "../images/armyband3.jpeg";
+import ensemble4 from "../images/armyband4.jpeg";
+import ensemble5 from "../images/armyband5.jpeg";
+import armyband7 from "../images/armyband7.jpeg";
+
+function changeImage1() {
+  document.getElementById("ensembleImage").src = ensemble1;
+}
+function changeImage2() {
+  document.getElementById("ensembleImage").src = ensemble2;
+}
+function changeImage3() {
+  document.getElementById("ensembleImage").src = ensemble3;
+}
+function changeImage4() {
+  document.getElementById("ensembleImage").src = ensemble4;
+}
+function changeImage5() {
+  document.getElementById("ensembleImage").src = ensemble5;
+}
 
 export class EnsemblesComponent extends Component {
   render() {
@@ -7,24 +29,44 @@ export class EnsemblesComponent extends Component {
         <h3 id="mainDivTitle">Ensembles</h3>
         <div className="ensembleChildContainer">
           <div className="ensembleNames">
-            <div className="ensembleName" id="ensemble1">
+            <div
+              className="ensembleName"
+              id="ensemble1"
+              onMouseOver={changeImage1}
+            >
+              ensemble 1
+            </div>
+            <div
+              className="ensembleName"
+              id="ensemble2"
+              onMouseOver={changeImage2}
+            >
               ensemble 2
             </div>
-            <div className="ensembleName" id="ensemble2">
+            <div
+              className="ensembleName"
+              id="ensemble3"
+              onMouseOver={changeImage3}
+            >
               ensemble 3
             </div>
-            <div className="ensembleName" id="ensemble3">
+            <div
+              className="ensembleName"
+              id="ensemble4"
+              onMouseOver={changeImage4}
+            >
               ensemble 4
             </div>
-            <div className="ensembleName" id="ensemble4">
+            <div
+              className="ensembleName"
+              id="ensemble5"
+              onMouseOver={changeImage5}
+            >
               ensemble 5
-            </div>
-            <div className="ensembleName" id="ensemble5">
-              ensemble 6
             </div>
           </div>
           <div className="ensembleImageContainer">
-            <div id="ensembleImage">changing image</div>
+            <img id="ensembleImage" alt="" src={armyband7} />
           </div>
         </div>
       </div>
