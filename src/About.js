2ec5a5165770cfa0b26877c27aecf167fa461c0d
Joe Young <musicjoeyoung@gmail.com>
2021-06-02 21:58:53 -0400

    more skeletal edits

--- src/About.js
+++ src/About.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import armyband3 from "./images/armyband3.jpeg";
+import bioImg from "./images/78tharmybandcircle.jpeg";
 
 export class About extends Component {
   render() {
@@ -20,11 +21,13 @@ export class About extends Component {
         <div className="history">
           <h2>History</h2>
           <p>1-2 sentences here</p>
-          <div className="meetTheBand">
-            <h2>Meet the Band</h2>
-            Include dropdown menu of sections. Name that changes via Redux
-            ("Command Staff", "Brass", "Percussion", etc.)
-          </div>
+        </div>
+        <div className="meetTheBand">
+          <h2>Meet the Band</h2>
+          Include dropdown menu of sections. Name that changes via Redux
+          ("Command Staff", "Brass", "Percussion", etc.)
+          <h3>Command Team</h3>
+          <img id="bioImg" src={bioImg} alt="" />
         </div>
         <Footer />
       </div>
