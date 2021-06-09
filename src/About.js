4e5d732cf061ff9f89439b3202bc8fc3190cc0e7
Joe Young <musicjoeyoung@gmail.com>
2021-06-09 11:55:02 -0400

    section components; about page

--- src/About.js
+++ src/About.js
@@ -4,38 +4,6 @@ import Footer from "./Footer";
 import armyband3 from "./images/armyband3.jpeg";
 import dropdownImg from "./images/dropdown.png";
 import Brass from "./AboutSections/Brass.js";
-/* import bioImg from "./images/78tharmybandcircle.jpeg";
-import newImg from "./images/armyband7.jpeg";
-import newImg2 from "./images/EndleinTestPic.png";
-import newImg3 from "./images/armyband1.jpg";
-import newImg4 from "./images/armyband2.jpeg"; */
-
-/* const bio1 =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!";
-const bio2 =
-  "I'm baby vHS church-key put a bird on it synth kogi. Fanny pack craft beer four dollar toast fam before they sold out messenger bag chambray slow-carb aesthetic trust fund chillwave mlkshk 3 wolf moon portland. Tofu godard 8-bit, fanny pack banh mi offal selfies blue bottle freegan chartreuse squid. Kinfolk yuccie fam tbh la croix taiyaki mlkshk literally sriracha art party chambray franzen. Listicle chia tbh shabby chic umami kickstarter.Glossier selfies tattooed, prism pickled vinyl single-origin coffee fam +1 umami scenester viral. Listicle ennui cronut fashion axe. Polaroid ethical whatever, brunch ennui yuccie single-origin coffee cliche 8-bit pop-up glossier palo santo umami edison bulb. Whatever biodiesel aesthetic cloud bread quinoa selvage gastropub cold-pressed yr hell of microdosing. Chillwave gastropub keffiyeh brunch leggings hoodie, poutine pitchfork 3 wolf moon fam pour-over. Quinoa truffaut microdosing actually fixie franzen umami squid heirloom schlitz pour-over. Chartreuse +1 knausgaard tumeric small batch plaid everyday carry post-ironic fixie pitchfork whatever. Occupy fam palo santo single-origin coffee unicorn, chillwave actually sartorial. Marfa gluten-free fingerstache, cray succulents copper mug meditation farm-to-table gochujang keffiyeh hot chicken chia williamsburg. Distillery post-ironic four dollar toast 8-bit 90's, bitters leggings paleo kitsch pour-over seitan blue bottle. Fanny pack leggings lomo chillwave echo park letterpress vice locavore whatever tousled beard literally stumptown. Glossier tumblr authentic, helvetica tousled ramps hoodie. Mumblecore fixie chartreuse normcore glossier lomo. Art party ethical microdosing pickled migas squid poke pour-over skateboard ramps sriracha tbh yr dreamcatcher.";
-const bio3 =
-  "Plaid raw denim la croix, thundercats etsy kitsch fingerstache proident roof party. Kombucha vaporware iceland, mumblecore copper mug kale chips VHS vice minim prism plaid meh. Chartreuse photo booth squid, hammock brunch prism veniam put a bird on it aliqua drinking vinegar umami. Mumblecore listicle cray squid lo-fi cold-pressed meditation gluten-free deep v chia shabby chic disrupt salvia. Ugh hammock narwhal tilde typewriter aliqua, 8-bit cronut jean shorts irure hell of letterpress selvage tempor. Quis non plaid cornhole, chillwave leggings officia ethical quinoa meditation dolore ugh whatever blue bottle ea. Duis chicharrones small batch quis wayfarers cornhole officia kogi pabst flannel kombucha fashion axe next level laboris. Portland direct trade mustache jianbing put a bird on it, poke culpa sriracha banjo chicharrones hot chicken excepteur. Sustainable bushwick pitchfork quinoa. Gochujang stumptown pickled jianbing cornhole unicorn. Shaman synth XOXO succulents dolor knausgaard kombucha photo booth tattooed. Shabby chic cloud bread pitchfork, chillwave in et affogato +1 direct trade. Migas pok pok echo park VHS commodo velit.";
-const bio4 =
-  "Meditation +1 eu iPhone, quis sunt tote bag ut vegan dolore organic tattooed. Normcore street art salvia mollit poke banh mi cred taiyaki. Disrupt shabby chic woke slow-carb chartreuse small batch knausgaard sint pop-up dreamcatcher. Street art listicle irony flannel enamel pin twee chambray jean shorts vaporware poutine craft beer woke. Knausgaard qui ethical do occaecat. Gluten-free letterpress aliquip, tumblr literally deep v vape post-ironic velit +1 butcher. Nostrud hammock vegan farm-to-table pour-over PBR&B, la croix thundercats man braid drinking vinegar kinfolk heirloom meggings schlitz readymade. Chambray officia iPhone chillwave schlitz.";
-
-function changeBioGroup1() {
-  document.getElementById("bioImg1").src = newImg;
-  document.getElementById("bioText1").innerText = bio1;
-  document.getElementById("bioImg2").src = newImg2;
-  document.getElementById("bioText2").innerText = bio2;
-}
-
-function changeBioGroup2() {
-  document.getElementById("bioImg1").src = newImg3;
-  document.getElementById("bioText1").innerText = bio3;
-  document.getElementById("bioImg2").src = newImg4;
-  document.getElementById("bioText2").innerText = bio4;
-} */
-
-function changeToBrass() {
-  document.getElementById("section").src = <Brass />;
-}
 
 export class About extends Component {
   render() {
@@ -60,12 +28,8 @@ export class About extends Component {
             <div className="dropdown">
               <img className="dropbtn" src={dropdownImg} alt="" />
               <div className="navbar">
-                <li id="navbarLinks" /* onClick={changeBioGroup1} */>
-                  Command Team
-                </li>
-                <li id="navbarLinks" onClick={changeToBrass}>
-                  Brass
-                </li>
+                <li id="navbarLinks">Command Team</li>
+                <li id="navbarLinks">Brass</li>
               </div>
             </div>
           </div>
@@ -73,15 +37,7 @@ export class About extends Component {
           Include dropdown menu of sections. Name that changes via Redux
           ("Command Staff", "Brass", "Percussion", etc.)
           <div id="section">
-            {/*  <h3>Command Team</h3>
-            <img id="bioImg1" src={bioImg} alt="" />
-            <p id="bioText1" src={bio1} />
-            <img id="bioImg2" src={bioImg} alt="" />
-            <p id="bioText2" src={bio1} />
-            <img id="bioImg3" src={bioImg} alt="" />
-            <p id="bioText3" src={bio1} />
-            <img id="bioImg4" src={bioImg} alt="" />
-            <p id="bioText4" src={bio1} /> */}
+            <Brass />
           </div>
         </div>
         <Footer />
---
+++ src/AboutSections/CommandTeam.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import bioImg from "../images/78tharmybandcircle.jpeg";
+
+const bio1 =
+  "tatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!";
+
+const bio2 = "Lorem ipsum dolor sit  elit. ";
+
+const bio3 = " molestiae quas vel sint commodi repudiandae consequuntur ";
+
+const bio4 =
+  "amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!";
+
+class CommandTeam extends Component {
+  render() {
+    return (
+      <div /* id="section" */>
+        <h3>Command Team</h3>
+        <img id="bioImg" src={bioImg} alt="" />
+        <p id="bioText">{bio1}</p>
+        <img id="bioImg" src={bioImg} alt="" />
+        <p id="bioText">{bio2}</p>
+        <img id="bioImg" src={bioImg} alt="" />
+        <p id="bioText">{bio3} </p>
+        <img id="bioImg" src={bioImg} alt="" />
+        <p id="bioText">{bio4} </p>
+      </div>
+    );
+  }
+}
+
+export default CommandTeam;
