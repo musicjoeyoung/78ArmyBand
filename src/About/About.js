6adaed293dc3bdd0dbfcd6f9739f09df71a3fdc6
Joe Young <musicjoeyoung@gmail.com>
2021-06-11 17:38:19 -0400

    about/profile edits

--- src/About/About.js
+++ src/About/About.js
@@ -4,6 +4,7 @@ import Footer from "../Footer";
 import Profiles from "./Profiles";
 import NavComponent from "./NavComponent";
 import { staff } from "./staffArray.js";
+import armyband3 from "../images/armyband3.jpeg";
 
 function About() {
   const [section, setSection] = useState("Command Team");
@@ -11,11 +12,20 @@ function About() {
 
   const profiles = staff.filter((staff) => staff.section === section);
 
-  //console.log({ section });
+  console.log({ section });
 
   return (
     <div>
       <Navbar />
+      <div className="about-page-pic-and-text">
+        <div className="about-page-pic">
+          <img src={armyband3} className="about-page-bg-image" alt="" />
+        </div>
+        <div className="about-page-bg-text">
+          <h1>About the band</h1>
+          <p id="about-page-pText">The 78th Army Band...</p>
+        </div>
+      </div>
       <div className="meetTheBand">
         <h1>Meet the Band!</h1>
         <NavComponent setSection={updateSection}></NavComponent>
