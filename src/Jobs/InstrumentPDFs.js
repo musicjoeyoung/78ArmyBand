970e00b4487a227ba5d1d0aaa4dc59d3e462980b
Joe Young <musicjoeyoung@gmail.com>
2021-06-29 11:54:04 -0400

    pdf setup

--- src/Jobs/InstrumentPDFs.js
+++ src/Jobs/InstrumentPDFs.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 /* import { Document, Page } from "react-pdf"; */
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
-import pdf from "./EuphFAQs.pdf";
+import pdf1 from "./AMPA Materials/9B/9B EXAMPLE QPP.pdf";
+import pdf2 from "./AMPA Materials/9B/9B Text.pdf";
+import pdf3 from "./AMPA Materials/9B/Part 2 AMPA 9B.pdf";
 
 //Instead of importing a PDF, do I import a list of the PDFs separated by instrument?
 //And then connect that if the value of an option (in NavComponent) is selected, then the PDF is then changed to that?
@@ -16,8 +18,13 @@ function InstrumentPDFs() {
 
   return (
     <div>
+      <div className="PDF-Options">
+        <div id="singlePDF-option">PDF 1</div>
+        <div id="singlePDF-option">PDF 2</div>
+        <div id="singlePDF-option">PDF 3</div>
+      </div>
       <Document
-        file={pdf}
+        file={pdf1}
         onLoadSuccess={({ numPages }) =>
           setNumPages(numPages) && onDocumentLoadSuccess({ numPages })
         }
