1fd4d13a176c86ec45ebaa80d5d09785b085f0b1
Joe Young <musicjoeyoung@gmail.com>
2021-11-30 20:16:09 -0500

    modals back to class

--- src/About/ModalTrigger.js
+++ src/About/ModalTrigger.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 
-const ModalTrigger = () => {
-  return (
-    <button
-      ref={this.props.buttonRef}
-      onClick={this.props.showModal}
-      className="modal-button"
-    >
-      {this.props.triggerText}
-    </button>
-  );
-};
+class ModalTrigger extends Component {
+  render() {
+    return (
+      <button
+        ref={this.props.buttonRef}
+        onClick={this.props.showModal}
+        className="modal-button"
+      >
+        {this.props.triggerText}
+      </button>
+    );
+  }
+}
 
 export default ModalTrigger;
