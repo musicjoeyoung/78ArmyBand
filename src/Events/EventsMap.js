fb0fb1632bc73debf92367f9db3d2646576b194f
Joe Young <musicjoeyoung@gmail.com>
2021-12-19 21:10:10 -0500

    this modal thing should be easier...

--- src/Events/EventsMap.js
+++ src/Events/EventsMap.js
@@ -3,7 +3,7 @@ import EventsModal from "./EventsModal";
 
 const EventsMap = ({ events }) => {
   const [show, setShow] = useState(false);
-  const [index, setIndex] = useState();
+  const [modalData, setModalData] = useState(null);
 
   return (
     <div>
@@ -14,39 +14,45 @@ const EventsMap = ({ events }) => {
           console.log("closed");
         }}
         show={show}
-        index={() => setIndex(index)}
       >
+        {/* <h1>{modalData.bio}</h1> */}
         <p>test {events.bio}</p>
       </EventsModal>
-
-      {events.map((events) => (
-        <div className="eventsChildContainer" key={events.id}>
-          <div className="eventsNames">
-            <div id="events">
-              <div
-                id="eventsAtag"
-                onClick={() => {
-                  setShow(true);
-                  setIndex(events.id);
-                  console.log("Bio:", events.bio, "ID:", events.id);
-                }}
-              >
-                <div id="date">
-                  <div id="Day">{events.day}</div>
-                  <div id="Month">{events.month}</div>
-                </div>
-                <div id="infoContainer">
-                  <h2 id="eventEnsemble">{events.ensemble}</h2>
-                  <h2 id="eventTitle">{events.name}</h2>
-                  <h3 id="eventLocation">{events.location}</h3>
-                  <p id="eventCity">{events.cityState}</p>
-                  <p id="eventTime">{events.time}</p>
+      <>
+        {events.map(
+          (event) => (
+            console.log("event.id", event.id),
+            (
+              <div className="eventsChildContainer" key={event.id}>
+                <div className="eventsNames">
+                  <div id="events">
+                    <div
+                      id="eventsAtag"
+                      onClick={() => {
+                        setShow(true);
+                        setModalData(event);
+                        console.log("Bio:", event.bio, "ID:", event.id);
+                      }}
+                    >
+                      <div id="date">
+                        <div id="Day">{event.day}</div>
+                        <div id="Month">{event.month}</div>
+                      </div>
+                      <div id="infoContainer">
+                        <h2 id="eventEnsemble">{event.ensemble}</h2>
+                        <h2 id="eventTitle">{event.name}</h2>
+                        <h3 id="eventLocation">{event.location}</h3>
+                        <p id="eventCity">{event.cityState}</p>
+                        <p id="eventTime">{event.time}</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      ))}
+            )
+          )
+        )}
+      </>
     </div>
   );
 };
