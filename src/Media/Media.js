d38463fb0f7b72e53b297aaf6ec7152839d18ea1
Joe Young <musicjoeyoung@gmail.com>
2023-01-16 18:06:34 -0500

    style

--- src/Media/Media.js
+++ src/Media/Media.js
@@ -17,7 +17,7 @@ const Media = () => {
             height="315"
             src="https://www.youtube.com/embed/64R69szw6d4"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
           ></iframe>
