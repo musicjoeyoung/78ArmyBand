33daf1c7b1507c410b706dffa0d1b9c7deb6d88c
Joe Young <musicjoeyoung@gmail.com>
2023-01-22 22:49:39 -0500

    style

--- src/Ensembles/Ensembles.js
+++ src/Ensembles/Ensembles.js
@@ -13,6 +13,12 @@ const Ensembles = () => {
   return (
     <div>
       <h1 className="mobileH1">Ensembles</h1>
+      <p id="ensemblePageDescriptionMobile">
+        The 78th Army Band's many ensembles perform throughout the larger
+        tri-state area for all communities. They play a vital role in the Army
+        by providing musical support for various troops, units and branches,
+        entertaining civilians, and serving as musical ambassadors of the Army.
+      </p>
       <NavComponent setName={updateName} />
       <div className="individualEnsemble">
         <EnsemblesProfiles ensembles={profiles} />
