920798ee31d46808e214f5cb4a4f5c676080fbe4
Joe Young <musicjoeyoung@gmail.com>
2023-01-25 21:37:15 -0500

    performance

--- src/HomeCarousel.js
+++ src/HomeCarousel.js
@@ -2,9 +2,6 @@ import React from "react";
 import "./App.css";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-import img1 from "./images/BandParade2.webp";
-import img2 from "./images/BandFormation.webp";
-import img3 from "./images/BandParadeNYC.webp";
 
 const HomeCarousel = () => {
   return (
@@ -14,9 +11,21 @@ const HomeCarousel = () => {
       infinite
       animationType="fadeout"
     >
-      <img src={img3} alt="" className="sliderimg" />
-      <img src={img1} alt="" className="sliderimg" />
-      <img src={img2} alt="" className="sliderimg" />
+      <img
+        src="https://res.cloudinary.com/dfbuwtl5q/image/upload/v1674700058/BandParadeNYC_v1cnja.webp"
+        alt=""
+        className="sliderimg"
+      />
+      <img
+        src="https://res.cloudinary.com/dfbuwtl5q/image/upload/v1674700059/BandParade2_sn83kd.webp"
+        alt=""
+        className="sliderimg"
+      />
+      <img
+        src="https://res.cloudinary.com/dfbuwtl5q/image/upload/v1674700058/BandFormation_ftintj.webp"
+        alt=""
+        className="sliderimg"
+      />
     </AliceCarousel>
   );
 };
