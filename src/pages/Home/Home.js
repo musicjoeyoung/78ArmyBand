8b38ed4f967307db184b32f6622acaf309ecadab
Joe Young <musicjoeyoung@gmail.com>
2023-08-26 21:47:25 -0400

    Home --> SCSS

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import HomeCarousel from "../../components/HomeCarousel/HomeCarousel";
 import parade from "/Users/musicjoeyoung/Desktop/Coding/Army/78armyband/src/assets/images/78MarchMobile.jpg";
+import "./Home.scss"
 
 const Home = () => {
   return (
     <>
-      <div className="imgContainer">
-        <img src={parade} className="mobileHomeImg" alt="Band marching" />
+      <div className="container">
+        <img src={parade} className="container__mobileHomeImg" alt="Band marching" />
         <img
           src="https://res.cloudinary.com/dfbuwtl5q/image/upload/v1674498250/78logo_1_fsqjox.png"
-          className="homePageLogoImg"
+          className="container__homePageLogoImg"
           alt="US Army Reserve; 78th Army Band; Fort Dix, NJ; Fort Totten, NY"
         />
       </div>
